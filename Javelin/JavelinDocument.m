ef84da32ee2d66186d15e41c1dd84f84b78b7b2a
harry <macmini@macharry.lan>
2011-08-27 18:33:46 +0200

    asads

--- Javelin/JavelinDocument.m
+++ Javelin/JavelinDocument.m
@@ -7,6 +7,7 @@
 //
 
 #import "JavelinDocument.h"
+#import "JavelinController.h"
 
 @implementation JavelinDocument
 
@@ -17,14 +18,57 @@
         // Add your subclass-specific initialization here.
         // If an error occurs here, send a [self release] message and return nil.
     }
+    _fileContents = nil;
     return self;
 }
+
 
 - (NSString *)windowNibName
 {
     // Override returning the nib file name of the document
     // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"JavelinDocument";
+}
+
+- (void) makeWindowControllers
+{
+    JavelinController	*controller;
+    
+    // Create controller.
+    controller = [[JavelinController alloc] initWithWindowNibName: [self windowNibName]];
+    [self addWindowController: controller];
+    
+    // Done.
+    [controller release];
+    
+    return;
+}
+
+/*
+- (BOOL) readFromURL:(NSURL *)url ofType:(NSString *)type
+{
+    //just check file type. The actual file read will be done in
+    //JavelinController
+    if ( [type caseInsensitiveCompare:@"pdf"] == NSOrderedSame )
+        return YES;
+    else if ( [type caseInsensitiveCompare:@"drmx"] == NSOrderedSame )
+        return YES;
+    
+    return NO;
+}
+*/
+
+
+- (BOOL) readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+{
+    if ( [typeName caseInsensitiveCompare:@"PDF Document"] == NSOrderedSame ) return YES;
+    if ( [typeName caseInsensitiveCompare:@"Javelin Document"] == NSOrderedSame ) return YES;
+    return NO;
+}
+
+- (NSAttributedString*)getFileContents
+{
+    return _fileContents;
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
@@ -43,16 +87,17 @@
     }
     return nil;
 }
-
+/*
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
-    /*
-    Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    */
+   
+//Insert code here to read your document from the given data of the specified type. 
+//If outError != NULL, ensure that you create and set an appropriate error when returning NO.
+//You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
+    
     if (outError) {
         *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
     }
     return YES;
 }
-
+*/
 @end
